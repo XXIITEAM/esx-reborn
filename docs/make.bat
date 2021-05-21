3836c5363036a9222a5bd3b6c307c2d6188a9a8e
Ark <arkseyonet@gmail.com>
2021-05-20 20:31:23 -0500

    fix(all): Fix non working github links in all files

--- docs/make.bat
+++ docs/make.bat
@@ -7,7 +7,7 @@ rem
 rem   You shall not use any piece of this software in a commercial product / service
 rem   You shall not resell this software
 rem   You shall not provide any facility to install this particular software in a commercial product / service
-rem   If you redistribute this software, you must link to ORIGINAL repository at https://github.com/ESX-Org/esx-reborn
+rem   If you redistribute this software, you must link to ORIGINAL repository at https://github.com/esx-framework/esx-reborn
 rem   This copyright should appear in every part of the project code
 
 @echo off
